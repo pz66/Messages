cf5ebf95ac8942b297d0442b721cf8676513c273
Peter Zhang <243451745@qq.com>
2026-08-31 19:53:42 +0800

    fix build issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ android {
         }
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
